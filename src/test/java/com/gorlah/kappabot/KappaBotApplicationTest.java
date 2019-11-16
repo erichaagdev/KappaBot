c1d5d154cf9f4fe4e06afa96fae90ee1d00158b6
ploober <ploober@users.noreply.github.com>
2019-11-16 16:02:47 -0600

    migrate from maven to gradle

--- src/test/java/com/gorlah/kappabot/KappaBotApplicationTest.java
+++ src/test/java/com/gorlah/kappabot/KappaBotApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-class KappaBotApplicationTests {
+class KappaBotApplicationTest {
 
 	@MockBean
 	DiscordBot discordBot;
