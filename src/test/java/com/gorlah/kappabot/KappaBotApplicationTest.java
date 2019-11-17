e5a69058e6f0f839a39e64c183fca9fc30fd4779
ploober <ploober@users.noreply.github.com>
2019-11-17 16:43:14 -0600

    improve test property set up

--- src/test/java/com/gorlah/kappabot/KappaBotApplicationTest.java
+++ src/test/java/com/gorlah/kappabot/KappaBotApplicationTest.java
@@ -4,8 +4,10 @@ import com.gorlah.kappabot.integration.discord.DiscordBot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KappaBotApplicationTest {
 
     @MockBean
