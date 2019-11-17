28e67184c771f7130522e87769042cf65aaeb516
Gorlah <eah0592@gmail.com>
2019-11-16 21:31:28 -0600

    Fix code style on some files

--- src/main/java/com/gorlah/kappabot/subcommand/SubcommandConfigurer.java
+++ src/main/java/com/gorlah/kappabot/subcommand/SubcommandConfigurer.java
@@ -4,6 +4,7 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -17,36 +18,36 @@ import java.util.function.Function;
 @Component
 @RequiredArgsConstructor
 class SubcommandConfigurer implements ApplicationListener<ContextRefreshedEvent> {
-
-  private final Set<Subcommand> subcommands;
-
-  @Override
-  public void onApplicationEvent(ContextRefreshedEvent event) {
-    var classMap = mapByClass(subcommands);
-    subcommands.stream()
-       .filter(subcommand -> subcommand.getClass() != RootCommand.class)
-       .forEach(subcommand -> classMap.get(subcommand.getParent()).addSubcommand(subcommand));
-    log.info("\nRegistered Commands:\n"
-       + new SubcommandRegistrationPrinter().print(classMap.get(RootCommand.class)));
-  }
-
-  private static <T> Map<Class, T> mapByClass(Collection<T> items) {
-    return items.stream().collect(ImmutableMap.toImmutableMap(Object::getClass, Function.identity()));
-  }
-
-  private static class SubcommandRegistrationPrinter {
-
-    String print(Subcommand subcommand) {
-      return print(subcommand, 1);
+    
+    private final Set<Subcommand> subcommands;
+    
+    @Override
+    public void onApplicationEvent(@NotNull ContextRefreshedEvent event) {
+        var classMap = mapByClass(subcommands);
+        subcommands.stream()
+                .filter(subcommand -> subcommand.getClass() != RootCommand.class)
+                .forEach(subcommand -> classMap.get(subcommand.getParent()).addSubcommand(subcommand));
+        log.info("\nRegistered Commands:\n"
+                + new SubcommandRegistrationPrinter().print(classMap.get(RootCommand.class)));
     }
-
-    private String print(Subcommand subcommand, int depth) {
-      var sb =
-         new StringBuilder(Strings.repeat("  ", depth) + "- "
-            + subcommand.getName() + ": " + subcommand.getHelpText() + "\n");
-      subcommand.getChildren().values()
-         .forEach(child -> sb.append(print(child, depth + 1)));
-      return sb.toString();
+    
+    private static <T> Map<Class, T> mapByClass(Collection<T> items) {
+        return items.stream().collect(ImmutableMap.toImmutableMap(Object::getClass, Function.identity()));
     }
-  }
+    
+    private static class SubcommandRegistrationPrinter {
+        
+        String print(Subcommand subcommand) {
+            return print(subcommand, 1);
+        }
+        
+        private String print(Subcommand subcommand, int depth) {
+            var sb =
+                    new StringBuilder(Strings.repeat("  ", depth) + "- "
+                            + subcommand.getName() + ": " + subcommand.getHelpText() + "\n");
+            subcommand.getChildren().values()
+                    .forEach(child -> sb.append(print(child, depth + 1)));
+            return sb.toString();
+        }
+    }
 }
