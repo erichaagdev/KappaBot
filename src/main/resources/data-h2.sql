14779bca42db374bc590ba0e1126fa0b43bbf6af
Gorlah <eah0592@gmail.com>
2019-11-29 22:42:13 -0600

    Add file to initialize h2 database for testing

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,4 @@
+INSERT INTO KB_IMAGE (KB_ALIAS, KB_URL, KB_USE_COUNT, KB_USER)
+VALUES ('freedomfriday', 'https://media.tenor.com/images/e622d5c1725caea5b1fc2aac8040e860/tenor.gif', 1, 'testuser1'),
+       ('itstreason', 'https://i.imgur.com/sJRP8hW.gif', 3, 'testuser2'),
+       ('itsfreerealestate', 'https://thumbs.gfycat.com/SparseBewitchedBuzzard-size_restricted.gif', 7, 'testuser1');
