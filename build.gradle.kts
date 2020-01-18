3ce498e7addfd1c62743bf563dab5273b1466b36
ploober <ploober@users.noreply.github.com>
2020-01-18 14:50:19 -0600

    apply project code style

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,23 +10,23 @@ buildscript {
 }
 
 plugins {
-  java
-  idea
-  id("io.franzbecker.gradle-lombok") version "3.1.0"
-  id("org.springframework.boot") version "2.2.1.RELEASE"
-  id("io.spring.dependency-management") version "1.0.8.RELEASE"
+    java
+    idea
+    id("io.franzbecker.gradle-lombok") version "3.1.0"
+    id("org.springframework.boot") version "2.2.1.RELEASE"
+    id("io.spring.dependency-management") version "1.0.8.RELEASE"
 }
 
 dependencyManagement {
-  imports {
-    mavenBom("org.springframework.boot:spring-boot-dependencies:2.2.1.RELEASE")
-  }
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:2.2.1.RELEASE")
+    }
 }
 
 repositories {
-  mavenLocal()
-  mavenCentral()
-  jcenter()
+    mavenLocal()
+    mavenCentral()
+    jcenter()
 }
 
 group = "com.gorlah"
@@ -35,37 +35,37 @@ version = "0.0.1-SNAPSHOT"
 description = "KappaBot: a Discord chat bot"
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_11
-  targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
 }
 
 tasks {
-  test {
-    useJUnitPlatform()
-  }
+    test {
+        useJUnitPlatform()
+    }
 }
 
 dependencies {
-  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-  implementation("org.springframework.boot:spring-boot-starter-web")
-  implementation("com.github.ben-manes.caffeine:caffeine:2.8.0")
-  implementation("net.dv8tion:JDA:4.0.0_39") {
-    exclude("club.minnced:opus-java")
-  }
-  implementation("org.jsoup:jsoup:1.10.2")
-  implementation("org.apache.commons:commons-text:1.8")
-  implementation("com.google.guava:guava:28.1-jre")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.0")
+    implementation("net.dv8tion:JDA:4.0.0_39") {
+        exclude("club.minnced:opus-java")
+    }
+    implementation("org.jsoup:jsoup:1.10.2")
+    implementation("org.apache.commons:commons-text:1.8")
+    implementation("com.google.guava:guava:28.1-jre")
 
-  // Database drivers
-  implementation("mysql:mysql-connector-java:8.0.18")
-  implementation("com.h2database:h2:1.4.200")
+    // Database drivers
+    implementation("mysql:mysql-connector-java:8.0.18")
+    implementation("com.h2database:h2:1.4.200")
 
-  // Test dependencies
-  testImplementation("org.springframework.boot:spring-boot-starter-test") {
-    exclude("org.junit.vintage:junit-vintage-engine")
-  }
+    // Test dependencies
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude("org.junit.vintage:junit-vintage-engine")
+    }
 
-  // Lombok
-  compileOnly("org.projectlombok:lombok:1.18.10")
-  annotationProcessor("org.projectlombok:lombok:1.18.10")
+    // Lombok
+    compileOnly("org.projectlombok:lombok:1.18.10")
+    annotationProcessor("org.projectlombok:lombok:1.18.10")
 }
